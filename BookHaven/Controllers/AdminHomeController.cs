de859c17b8791dda2e7e573e57e3400d31f58761
Maxim Subotin <maxim.sub21@gmail.com>
2024-02-26 19:21:50 +0200

    Fixing more stuff with views

--- BookHaven/Controllers/AdminHomeController.cs
+++ BookHaven/Controllers/AdminHomeController.cs
@@ -2,7 +2,7 @@
 
 namespace BookHaven.Controllers
 {
-    public class AdminController : Controller
+    public class AdminHomeController : Controller
     {
         public IActionResult Index()
         {
