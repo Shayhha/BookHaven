2be91e0d9c741bb52bca4fe017446b9ecb52a5db
Maxim Subotin <maxim.sub21@gmail.com>
2024-02-27 11:16:55 +0200

    adding login basic functionality

--- BookHeaven/Models/Login.cs
+++ BookHeaven/Models/Login.cs
@@ -1,6 +1,25 @@
-﻿namespace BookHaven.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace BookHeaven.Models
 {
     public class Login
     {
+        [Required(ErrorMessage = "email is required")]
+        [RegularExpression("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", ErrorMessage = "email must follow a generic email pattern.")]
+        public string email { get; set; }
+
+        [Required(ErrorMessage = "password is required")] 
+        [RegularExpression("^(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d!@#$%^&*()-_=+{}\\[\\]|;:'\",.<>?]{6,12}$",
+            ErrorMessage = "password must have at-least one cappital letter, at-least one number and be between 6 and 12 chars long.")]
+        public string password { get; set; }
+
+        public Login() { }
+
+        public Login(string _email, string _password)
+        {
+            email = _email;
+            password = _password;
+        }
     }
 }
