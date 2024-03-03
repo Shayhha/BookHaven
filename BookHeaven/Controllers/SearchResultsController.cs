73f891b16560a0885127fa3cabf97955120e3f13
Maxim Subotin <maxim.sub21@gmail.com>
2024-03-03 21:48:20 +0200

    making the search bar work with ui

--- BookHeaven/Controllers/SearchResultsController.cs
+++ BookHeaven/Controllers/SearchResultsController.cs
@@ -10,6 +10,18 @@ namespace BookHeaven.Controllers
             SearchResults searchResults = new SearchResults(searchQuery);
 
             // some sql here to find all the elements that fit the search query?
+            searchResults = SQLHelper.SQLSearchBook(searchResults);
+            if (searchResults.books.Count() > 0)
+            {
+                foreach (Book book in searchResults.books)
+                {
+                    Console.WriteLine(book.name);
+                }
+            } else
+            {
+                Console.WriteLine("empty list");
+            }
+                
 
             return View("SearchResultsView", searchResults);
         }
