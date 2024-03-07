fe84ebcfd630d62faa8540016759bbe3df271906
Maxim Subotin <maxim.sub21@gmail.com>
2024-03-07 19:40:57 +0200

    adding a function to check the edit user profile input

--- BookHeaven/Controllers/ProfileController.cs
+++ BookHeaven/Controllers/ProfileController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,6 +16,35 @@ namespace BookHeaven.Controllers
         public IActionResult showSettingsView()
         {
             return View("SettingsView");
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> SaveChanges()
+        {
+            using (StreamReader reader = new StreamReader(Request.Body))
+            {
+                string jsonString = await reader.ReadToEndAsync();
+                dynamic data = JsonConvert.DeserializeObject(jsonString);
+
+                if (data != null)
+                {
+                    // Here we check what data needs to be updated and call the appropriate sql methods
+
+                    // You can access the data like so:
+                    //string email = data.email;
+                    //Console.WriteLine("email = " + email);
+
+
+                    return Json(new { success = true });
+                }
+                else
+                {
+                    return Json(new { success = false });
+                }
+            }
+
         }
 
         [HttpPost]
