638edef3fd9f606a16fd08cb9131a9532e6b6dc9
Shay Hahiashvili <shayhh@outlook.com>
2024-03-15 20:00:03 +0200

    g

--- BookHeaven/Models/Book.cs
+++ BookHeaven/Models/Book.cs
@@ -8,12 +8,12 @@ namespace BookHeaven.Models
         public int bookId { get; set; }
 
         [Required(ErrorMessage = "Name is required")]
-        [RegularExpression("^[A-Za-z]*$", ErrorMessage = "Name must only contain letters")]
+        [RegularExpression("^[A-Za-z ]+$", ErrorMessage = "Name must only contain letters")]
         [MaxLength(255, ErrorMessage = "Name must be between 1 and 255 characters")]
         public string name { get; set; }
 
         [Required(ErrorMessage = "Author is required")]
-        [RegularExpression("^[A-Za-z]*$", ErrorMessage = "Author must only contain letters and spaces")]
+        [RegularExpression("^[A-Za-z ]+$", ErrorMessage = "Author must only contain letters and spaces")]
         [MaxLength(255, ErrorMessage = "Author must be between 1 and 255 characters")]
         public string author { get; set; }
 
@@ -26,7 +26,7 @@ namespace BookHeaven.Models
         public string category { get; set; }
 
         [Required(ErrorMessage = "Format is required")]
-        [RegularExpression("^[A-Za-z]*$", ErrorMessage = "Format must only contain letters")]
+        [RegularExpression("^[A-Za-z]+$", ErrorMessage = "Format must only contain letters")]
         [MaxLength(255, ErrorMessage = "Format must be between 1 and 255 characters")]
         public string format { get; set; }
 
