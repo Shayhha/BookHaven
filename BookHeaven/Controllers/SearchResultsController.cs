5ed94795806749d0ada3c4a1a93e84ec4cc70d46
Maxim Subotin <maxim.sub21@gmail.com>
2024-03-16 21:11:25 +0200

    adding the filter button and making it show the correct text and pages. only need the filter functions now

--- BookHeaven/Controllers/SearchResultsController.cs
+++ BookHeaven/Controllers/SearchResultsController.cs
@@ -1,11 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using BookHeaven.Models;
 using System.Text.RegularExpressions;
+using BookHeaven.Extensions;
 
 namespace BookHeaven.Controllers
 {
     public class SearchResultsController : Controller
     {
+        // this part is for the session variables
+        private readonly IHttpContextAccessor _contx;
+
+        public SearchResultsController(IHttpContextAccessor contx)
+        {
+            _contx = contx;
+        }
+        // # # #
+
+
         public IActionResult showSearchResults(string searchQuery)
         {
             if (searchQuery == null) return StatusCode(204); // tells the browser that the request was successful but there is no content to display.
@@ -17,6 +28,8 @@ namespace BookHeaven.Controllers
             else
                 searchResults = SQLHelper.SQLSearchBook(searchResults);
 
+            _contx.HttpContext.Session.SetObjectAsJson("listOfBooks", searchResults.books); // Store the book list in session
+
             return View("SearchResultsView", searchResults);
         }
 
@@ -24,6 +37,8 @@ namespace BookHeaven.Controllers
         {
             SearchResults searchResults = new SearchResults(searchQuery);
             searchResults = SQLHelper.SQLSearchCategory(searchResults);
+
+            _contx.HttpContext.Session.SetObjectAsJson("listOfBooks", searchResults.books); // Store the book list in session
 
             return View("SearchResultsView", searchResults);
         }
@@ -37,6 +52,39 @@ namespace BookHeaven.Controllers
         public IActionResult showBookDetails(int bookId)
         {
             return RedirectToAction("showBookEdit", "Book", new { bookId = bookId });
+        }
+
+
+
+
+        public IActionResult filterBooks(string filterBy, string searchQuery = "")
+        {
+            SearchResults searchResults;
+
+            if (searchQuery == "")
+                searchResults = new SearchResults(", Filtered by: " + filterBy);
+            else
+                searchResults = new SearchResults(searchQuery.Split(",")[0] + ", Filtered by: " + filterBy);
+
+            // Get the books that we have saved in the session variable
+            List<Book> listOfBooks = _contx.HttpContext.Session.GetObjectFromJson<List<Book>>("listOfBooks");
+
+            // Call a function that will filter the sessionBooks
+            listOfBooks = filterTheBooks(listOfBooks);
+
+            // Then we add the books in the correct order to the searchResults object and send it to the users screen
+            foreach (Book book in listOfBooks)
+            {
+                searchResults.books.Add(book);
+            }
+
+            ViewBag.SelectedFilter = filterBy;
+            return View("SearchResultsView", searchResults);
+        }
+
+        public List<Book> filterTheBooks(List<Book> listOfBooks) // temporary function, will be replaced by other filter functions
+        {
+            return listOfBooks;
         }
     }
 }
