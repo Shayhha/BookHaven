71c30ef1183892bafe4d554e7456efe6a927d20b
Shay Hahiashvili <shayhh@outlook.com>
2024-03-22 12:57:56 +0200

    updated searchResultsController wit h our filter options, now should work with multiple filter options.

--- BookHeaven/Controllers/SearchResultsController.cs
+++ BookHeaven/Controllers/SearchResultsController.cs
@@ -68,7 +68,7 @@ namespace BookHeaven.Controllers
             List<Book> bookList = _contx.HttpContext.Session.GetObjectFromJson<List<Book>>("listOfBooks");
 
             // Call a function that will filter the sessionBooks
-            bookList = filterTheBooks(bookList, filterBy, searchResults.searchQuery, isCategory);
+            bookList = multiFilterBooks(bookList, filterBy, searchResults.searchQuery, isCategory);
 
             // Then we add the books in the correct order to the searchResults object and send it to the users screen
             foreach (Book book in bookList)
@@ -80,9 +80,36 @@ namespace BookHeaven.Controllers
             return View("SearchResultsView", searchResults);
         }
 
-        public List<Book> filterTheBooks(List<Book> bookList, string filterBy, string searchQuery = "", int isCategory = 0) // temporary function, will be replaced by other filter functions
+        /// <summary>
+        /// Function for sorting given bookList, filters by each filter option in filterBy: "A - Z,Paperback,10$ - 20$" etc.
+        /// </summary>
+        /// <param name="bookList"></param>
+        /// <param name="filterBy"></param>
+        /// <param name="searchQuery"></param>
+        /// <param name="isCategory"></param>
+        /// <returns></returns>
+        public List<Book> multiFilterBooks(List<Book> bookList, string filterBy, string searchQuery = "", int isCategory = 0) 
         {
-            switch (filterBy)
+            List<Book> tempBookList = bookList; //save bookList in temp list for sorting
+            List<string> filterList = filterBy.Split(',').ToList(); //create a new list from filterBy string
+            foreach (string filter in filterList) //iterate over list and filter our temp book list with appropriate filters
+            {
+                filterBooks(tempBookList, filter, searchQuery, isCategory); //call helper function for sorting by each filter
+            }
+            return tempBookList; //return filtered list
+        }
+
+        /// <summary>
+        /// Function that receives bookList and filters it by the given filter
+        /// </summary>
+        /// <param name="bookList"></param>
+        /// <param name="filter"></param>
+        /// <param name="searchQuery"></param>
+        /// <param name="isCategory"></param>
+        /// <returns></returns>
+        public List<Book> filterBooks(List<Book> bookList, string filter, string searchQuery = "", int isCategory = 0) 
+        {
+            switch (filter) 
             {
                 case "A - Z":
                     return bookList.OrderBy(book => book.name).ToList();
@@ -100,13 +127,13 @@ namespace BookHeaven.Controllers
                     return bookList.Where(book => int.Parse(book.date.Split('/')[2]) >= 1950 && int.Parse(book.date.Split('/')[2]) <= 2000).ToList();
                 case "After 2000":
                     return bookList.Where(book => int.Parse(book.date.Split('/')[2]) > 2000).ToList();
-                case "Less Than $10": 
+                case "Less Than $10":
                     return bookList.Where(book => book.salePrice > 0 ? (book.salePrice < 10) : (book.price < 10)).ToList();
                 case "$10 - $20":
                     return bookList.Where(book => book.salePrice > 0 ? (book.salePrice >= 10 && book.salePrice <= 20) : (book.price >= 10 && book.price <= 20)).ToList();
-                case "$20 - $30": 
-                    return bookList.Where(book => book.salePrice > 0 ? (book.salePrice >= 20 && book.salePrice <= 30) : (book.price >= 20 && book.price <= 30)).ToList(); 
-                case "More Than $30": 
+                case "$20 - $30":
+                    return bookList.Where(book => book.salePrice > 0 ? (book.salePrice >= 20 && book.salePrice <= 30) : (book.price >= 20 && book.price <= 30)).ToList();
+                case "More Than $30":
                     return bookList.Where(book => book.salePrice > 0 ? (book.salePrice > 30) : (book.price > 30)).ToList();
                 case "Below 10":
                     return bookList.Where(book => book.ageLimitation < 10).ToList();
@@ -123,6 +150,12 @@ namespace BookHeaven.Controllers
             }
         }
 
+        /// <summary>
+        /// Function that return list of popular books sorted from most popular to least popular
+        /// </summary>
+        /// <param name="searchQuery"></param>
+        /// <param name="isCategory"></param>
+        /// <returns></returns>
         public List<Book> sortByMostPopular(string searchQuery, int isCategory)
         {
             if (isCategory == 0)
