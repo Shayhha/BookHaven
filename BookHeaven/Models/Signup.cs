d98291cf5a44907e4a9e22128cfdc425b77cb619
Maxim Subotin <maxim.sub21@gmail.com>
2024-03-24 19:44:34 +0200

    small fix

--- BookHeaven/Models/Signup.cs
+++ BookHeaven/Models/Signup.cs
@@ -4,20 +4,20 @@ namespace BookHeaven.Models
 {
     public class Signup
     {
-        [Required(ErrorMessage = "email is required")]
-        [RegularExpression("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", ErrorMessage = "email must follow a generic email pattern.")]
+        [Required(ErrorMessage = "Email is required.")]
+        [RegularExpression("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", ErrorMessage = "Email must follow a generic email pattern.")]
         public string email { get; set; }
 
-        [Required(ErrorMessage = "password is required")]
+        [Required(ErrorMessage = "Password is required")]
         [RegularExpression("^(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d!@#$%^&*()-_=+{}\\[\\]|;:'\",.<>?]{6,12}$",
-            ErrorMessage = "password must have at-least one capital letter, at-least one number and be between 6 and 12 chars long.")]
+            ErrorMessage = "Password must have at-least one capital letter, at-least one number and be between 6 and 12 chars long.")]
         public string password { get; set; }
 
-        [Required(ErrorMessage = "first name is required")]
+        [Required(ErrorMessage = "First name is required")]
         [RegularExpression("^[a-zA-Z]{2,20}$", ErrorMessage = "The input must contain only letters (a-z or A-Z) and be between 2 and 20 characters long.")]
         public string firstName { get; set; }
 
-        [Required(ErrorMessage = "last name is required")]
+        [Required(ErrorMessage = "Last name is required")]
         [RegularExpression("^[a-zA-Z]{2,20}$", ErrorMessage = "The input must contain only letters (a-z or A-Z) and be between 2 and 20 characters long.")]
         public string lastName { get; set; }
 
